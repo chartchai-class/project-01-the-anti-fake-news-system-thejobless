dd4deefe5d695847f615381c4380cb70f1966eec
Nawapon <nawapon_s@cmu.ac.th>
2025-08-26 11:53:06 +0700

    Add @vercel/analytics dependency and integrate analytics with Speed Insights in main.js for improved performance monitoring.

--- thejobless/src/main.js
+++ thejobless/src/main.js
@@ -3,9 +3,13 @@ import { createPinia } from "pinia"
 import router from "./router"
 import "./style.css"
 import App from "./App.vue"
-import { SpeedInsights } from "@vercel/speed-insights/vue"
+
+import { inject as injectAnalytics } from "@vercel/analytics"
 import { injectSpeedInsights } from "@vercel/speed-insights"
 
-if (import.meta.env.PROD) injectSpeedInsights()
+if (import.meta.env.PROD) {
+  injectAnalytics()
+  injectSpeedInsights()
+}
 
 createApp(App).use(createPinia()).use(router).mount("#app")
